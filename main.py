2b0d4077091b561d1008f5896ec36a6badbc3791
0nfiiree06 <0nfiiree06@gmail.com>
2026-02-13 15:53:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 from fastapi import FastAPI
+import psycopg2
+import os
 
 app = FastAPI()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+def get_connection():
+    return psycopg2.connect(DATABASE_URL)
 
 @app.get("/")
 def home():
     return {"mensaje": "API Mines funcionando"}
 
+@app.get("/dbtest")
+def db_test():
+    try:
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT 1;")
+        result = cursor.fetchone()
+        cursor.close()
+        conn.close()
+        return {"database": "conectada", "resultado": result}
+    except Exception as e:
+        return {"error": str(e)}
