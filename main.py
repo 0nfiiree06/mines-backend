56147bb264a86a084acf00f6a934a2dda08db029
0nfiiree06 <0nfiiree06@gmail.com>
2026-02-14 22:25:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI
 import psycopg2
+from psycopg2 import pool
 import os
 
 app = FastAPI()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+connection_pool = pool.SimpleConnectionPool(
+    1,
+    10,
+    DATABASE_URL
+)
+
 def get_connection():
-    return psycopg2.connect(DATABASE_URL)
+    return connection_pool.getconn()
+
+def release_connection(conn):
+    connection_pool.putconn(conn)
 
 @app.get("/")
 def home():
@@ -21,7 +31,8 @@ def db_test():
         cursor.execute("SELECT 1;")
         result = cursor.fetchone()
         cursor.close()
-        conn.close()
+        release_connection(conn)
         return {"database": "conectada", "resultado": result}
     except Exception as e:
         return {"error": str(e)}
+
